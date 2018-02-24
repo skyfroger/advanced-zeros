b9dea12d0115a656d274119cf16cfe65c243d42c
skyfroger <skyfroger@gmail.com>
2018-02-24 13:03:38 +0300

    Problem solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var min = (x, y) => (x > y ? y : x);
+
+  var noz = number;
+  var j = base;
+
+  for(var i = 2; i <= base; i++){
+      if(j % i == 0){
+          var p = 0;
+          while(j % i == 0){
+              p++;
+              j = Math.floor(j / i); // possible error
+          }
+          var c = 0;
+          var k = number;
+          while(k / i > 0){
+              c += Math.floor(k / i); // possible error
+              k = Math.floor(k / i);
+          }
+          noz = min(noz, Math.floor(c / p));
+      }
+  }
+
+  return noz;
 }
